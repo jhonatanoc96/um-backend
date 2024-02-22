bd054eb705d94e4618053dab897e66f8d3326dce
Jhonatan Ocampo <jhonatanocar96@gmail.com>
2024-02-22 11:33:58 -0500

    Solve server start

--- environment/constants.js
+++ environment/constants.js
@@ -1,5 +1,5 @@
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/um-database?readPreference=primary&directConnection=true&ssl=false&authSource=admin&retryWrites=true&w=majority';
-const HOST = 'localhost';
+const HOST = '127.0.0.1';
 
 module.exports = { 
     MONGODB_URI,
